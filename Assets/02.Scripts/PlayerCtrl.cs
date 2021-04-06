732ae16ac1a63759ecb81156ba8855573eec2c4e
Lee Jae Hyun <myleje@gmail.com>
2021-04-06 11:29:23 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -21,6 +21,19 @@ public class PlayerCtrl : MonoBehaviour
         h = Input.GetAxis("Horizontal");  // -1.0f ~ 0.0f ~ +1.0f
         v = Input.GetAxis("Vertical");    // -1.0f ~ 0.0f ~ +1.0f
 
+        //transform.position += new Vector3(0, 0, 0.1f);
+        transform.position += Vector3.forward * 0.1f; // (0, 0, 1) * 0.1f = (0, 0, 0.1f)
+
+        /* 정규화 벡터(Normalized Vector), 단위 벡터(Unit Vector)
+            
+            Vector3.forward == new Vector3(0, 0, 1)
+            Vector3.up      == new Vector3(0, 1, 0)
+            Vector3.right   == new Vector3(1, 0, 0)
+
+            Vector3.one     == new Vector3(1, 1, 1)
+            Vector3.zero    == new Vector3(0, 0, 0)
+        */
+
         Debug.Log($"h={h} / v={v}");
     }
 }
