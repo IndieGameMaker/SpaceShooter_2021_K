b4f5e31570efa67f08f7bb8aabb03dba5842abf5
Lee Jae Hyun <myleje@gmail.com>
2021-04-06 11:55:38 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -24,9 +24,17 @@ public class PlayerCtrl : MonoBehaviour
         //transform.position += new Vector3(0, 0, 0.1f);
         //transform.position += Vector3.forward * 0.1f; // (0, 0, 1) * 0.1f = (0, 0, 0.1f)
 
-        transform.Translate(Vector3.forward * 0.1f * v, Space.Self);
-        transform.Translate(Vector3.right * 0.1f * h);
-        
+        // transform.Translate(Vector3.forward * 0.1f * v, Space.Self);
+        // transform.Translate(Vector3.right * 0.1f * h);
+
+        //(전후방향 벡터) + (좌우방향의 벡터)
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        transform.Translate(moveDir.normalized * 0.1f);
+
+        Debug.Log($"moveDir={moveDir.magnitude}");
+        Debug.Log($"moveDir.normal={moveDir.normalized.magnitude}");
+
         /* 정규화 벡터(Normalized Vector), 단위 벡터(Unit Vector)
 
         Vector3.forward == new Vector3(0, 0, 1)
@@ -37,6 +45,6 @@ public class PlayerCtrl : MonoBehaviour
         Vector3.zero    == new Vector3(0, 0, 0)
         */
 
-        Debug.Log($"h={h} / v={v}");
+        //Debug.Log($"h={h} / v={v}");
     }
 }
