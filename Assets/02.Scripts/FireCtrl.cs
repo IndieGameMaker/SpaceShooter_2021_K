86c9fc02d737d2ca1fc274c04829cf66e1f8799f
Lee Jae Hyun <myleje@gmail.com>
2021-04-12 14:53:15 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    AudioListener : 1
+
+    AudioSource   : n
+
+*/
+
 public class FireCtrl : MonoBehaviour
 {
     public Transform firePos;
     public GameObject bulletPrefab;
 
+    public new AudioSource audio;
+    public AudioClip fireSfx;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();    
     }
 
     // Update is called once per frame
@@ -18,9 +28,19 @@ public class FireCtrl : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //동적으로 프리팹을 복제하는 함수
-            //Instantiate(생성할 객체, 위치값, 회전값)
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }
+    }
+
+    void Fire()
+    {
+        //동적으로 프리팹을 복제하는 함수
+        //Instantiate(생성할 객체, 위치값, 회전값)
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+
+        //총소리 발생
+        // audio.clip = fireSfx;
+        // audio.Play();
+        audio.PlayOneShot(fireSfx , 0.8f);
     }
 }
