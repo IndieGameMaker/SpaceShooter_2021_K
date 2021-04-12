45c5af34afdda9aabd037f16d6b947b613d18764
Lee Jae Hyun <myleje@gmail.com>
2021-04-12 11:24:20 +0900

    .

---
+++ Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //글로벌 좌표계 기준으로 힘을 가하는 함수
+        rb.AddForce(Vector3.forward * 1000.0f);
+    }
+
+}
