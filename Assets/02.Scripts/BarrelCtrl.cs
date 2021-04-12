933e97a659db37cfecba01ab3f5f43e3ebd12fff
Lee Jae Hyun <myleje@gmail.com>
2021-04-12 15:34:03 +0900

    .

---
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+    /*
+        C# Standard Naming Rule
+
+        Camel Case (카멜 케이스)   
+        : nickName, userName, playerID
+        : 변수(필드)
+
+        Pascal Case(파스칼 케이스)
+        : GetComponent<T>, SetPlayerID()
+        : 클래스 명, 구조체 명, 메소드(함수), C# 프로퍼티
+    */
+
+    private int hitCount = 0;
+
+
+}
