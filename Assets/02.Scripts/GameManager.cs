d7d57a0e5d7a4e248580e7c2ab56edb553132e73
Lee Jae Hyun <myleje@gmail.com>
2021-04-15 13:37:06 +0900

    .

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform[] points;
+    public GameObject monsterPrefab;
+
+    void Start()
+    {
+        points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
