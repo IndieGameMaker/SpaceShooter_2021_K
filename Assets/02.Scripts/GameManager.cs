6424fe232ad0bdc5b3739191f37886900a10099c
Lee Jae Hyun <myleje@gmail.com>
2021-04-15 14:40:39 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     //몬스터의 생성 주기(sec)
     public float createTime = 3.0f;
 
+    public bool isGameOver = false;
+
     void Start()
     {
         //List에 저장할 경우
@@ -20,26 +22,35 @@ public class GameManager : MonoBehaviour
         monsterPrefab = Resources.Load<GameObject>("monster");
         
         //(호출할_함수, 지연시간, 반복시간)
-        InvokeRepeating("CreateMonster", 2.0f, createTime);
+        //InvokeRepeating("CreateMonster", 2.0f, createTime);
         //CancelInvoke("CreateMonster");
+        
+        StartCoroutine(CreateMonster());
     }
 
-    void CreateMonster()
+    IEnumerator CreateMonster()
     {
-        //난수 발생
-        int idx = Random.Range(1, points.Count);
-        //몬스터를 생성
-        GameObject monster = Instantiate<GameObject>(monsterPrefab);
-        monster.name = "Monster";
-        //몬스터의 위치설정
-        monster.transform.position = points[idx].position;
-        
-        //벡터의 뺄셈연산 = (원점 - 몬스터의좌표)
-        Vector3 dir = points[0].position - points[idx].position;
-        //벡터를 이용해 쿼터니언 타입의 각도를 산출
-        Quaternion rot = Quaternion.LookRotation(dir);
-        //몬스터의 각도를 설정
-        monster.transform.rotation = rot;
+        yield return new WaitForSeconds(2.0f);
+
+        while (!isGameOver)
+        {
+            //난수 발생
+            int idx = Random.Range(1, points.Count);
+            //몬스터를 생성
+            GameObject monster = Instantiate<GameObject>(monsterPrefab);
+            monster.name = "Monster";
+            //몬스터의 위치설정
+            monster.transform.position = points[idx].position;
+            
+            //벡터의 뺄셈연산 = (원점 - 몬스터의좌표)
+            Vector3 dir = points[0].position - points[idx].position;
+            //벡터를 이용해 쿼터니언 타입의 각도를 산출
+            Quaternion rot = Quaternion.LookRotation(dir);
+            //몬스터의 각도를 설정
+            monster.transform.rotation = rot;
+
+            yield return new WaitForSeconds(createTime);
+        }
     }
 
 }
